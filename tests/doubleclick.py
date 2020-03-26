1bc4d7c394d9b9962027a40c35fe84b24e1e03a5
sebbastiannn <59449017+sebbastiannn@users.noreply.github.com>
2020-03-26 19:55:47 +0100

    double click on a box opens new window

--- tests/doubleclick.py
+++ tests/doubleclick.py
@@ -1,50 +1,44 @@
-import sys
-from PyQt5.QtGui import *
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
-class QDoublePushButton(QPushButton):
-    doubleClicked = pyqtSignal()
-    clicked = pyqtSignal()
-
-    def __init__(self, *args, **kwargs):
-        QPushButton.__init__(self, *args, **kwargs)
-        self.timer = QTimer()
+class ClickHandler():
+    def __init__(self, time):
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(time)
         self.timer.setSingleShot(True)
-        self.timer.timeout.connect(self.clicked.emit)
-        super().clicked.connect(self.checkDoubleClick)
+        self.timer.timeout.connect(self.timeout)
+        self.click_count = 0
 
-    @pyqtSlot()
-    def checkDoubleClick(self):
-        if self.timer.isActive():
-            self.doubleClicked.emit()
-            self.timer.stop()
-        else:
-            self.timer.start(250)
+    def timeout(self):
+        if self.click_count == 1:
+            print('Single click')
+        elif self.click_count > 1:
+            print('Double click')
+        self.click_count = 0
 
-class Window(QWidget):
-    def __init__(self, *args, **kwargs):
-        QWidget.__init__(self, *args, **kwargs)
+    def __call__(self):
+        self.click_count += 1
+        if not self.timer.isActive():
+            self.timer.start()
 
-        self.button = QDoublePushButton("Test", self)
-        self.button.clicked.connect(self.on_click)
-        self.button.doubleClicked.connect(self.on_doubleclick)
 
-        self.layout = QHBoxLayout()
-        self.layout.addWidget(self.button)
+class MyDialog(QWidget):
+    def __init__(self, parent=None):
+        super(MyDialog, self).__init__(parent)
 
-        self.setLayout(self.layout)
-        self.resize(120, 50)
-        self.show()
+        self.button1 = QtGui.QPushButton("Button 1")
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(self.button1)
+        self.setLayout(hbox)
 
-    @pyqtSlot()
-    def on_click(self):
-        print("Click")
+        self.click_handler = ClickHandler(300)
+        self.button1.clicked.connect(self.click_handler)
 
-    @pyqtSlot()
-    def on_doubleclick(self):
-        print("Doubleclick")
-
-app = QApplication(sys.argv)
-win = Window()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    w = MyDialog()
+    w.show()
+    sys.exit(app.exec_())
--- tests/testtoolbar/editor_graphics_view.py
+++ tests/testtoolbar/editor_graphics_view.py
@@ -1,11 +1,25 @@
-from PyQt5.QtWidgets import QGraphicsView
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5 import QtCore, QtGui
 
-class GraphicsView(QGraphicsView):
+from tests.testtoolbar.editor_graphics_box import GraphicsBox
+from tests.testtoolbar.editor_box_attributes import BoxAttributes
+
+class GraphicsView(QGraphicsView, QWidget):
+    left_clicked = QtCore.pyqtSignal(int)
+    right_clicked = QtCore.pyqtSignal(int)
     def __init__(self, grScene, parent=None):
         super().__init__(parent)
         self.grScene = grScene
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(250)
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.timeout)
+        self.left_click_count = 0
+        self.right_click_count = 0
+
 
         self.initUI()
 
@@ -31,6 +45,13 @@ class GraphicsView(QGraphicsView):
         else:
             super().mousePressEvent(event)
 
+    def timeout(self):
+        if self.left_click_count >= self.right_click_count:
+            self.left_clicked.emit(self.left_click_count)
+        else:
+            self.right_clicked.emit(self.right_click_count)
+        self.left_click_count = self.right_click_count = 0
+
     "what happen when we release mouse button"
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.MiddleButton:
@@ -51,10 +72,26 @@ class GraphicsView(QGraphicsView):
 
     "LEFT MOUSE BUTTON EVENT"
     def leftMouseButtonPress(self, event):
+        item = self.getItemAtClick(event)
+
         super().mousePressEvent(event)
 
     def leftMouseButtonRelease(self, event):
+        item = self.getItemAtClick(event)
+        if type(item) is GraphicsBox:
+            self.left_click_count += 1
+            if not self.timer.isActive():
+                self.timer.start()
+            if self.left_click_count <= 1:
+                print("click")
+            else:
+                print("double click")
+                self.box_Attribut = BoxAttributes()
+
+
+
         super().mouseReleaseEvent(event)
+
 
     "RIGHT MOUSE BUTTON EVENT"
     def rightMouseButtonPress(self, event):
@@ -62,3 +99,29 @@ class GraphicsView(QGraphicsView):
 
     def rightMouseButtonRelease(self, event):
         super().mouseReleaseEvent(event)
+
+    def getItemAtClick(self, event):
+        """ return the object on which we've clicked/release mouse button """
+        pos = event.pos()
+        obj = self.itemAt(pos)
+        return obj
+
+class ClickHandler():
+    def __init__(self, time):
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(time)
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.timeout)
+        self.click_count = 0
+
+    def timeout(self):
+        if self.click_count == 1:
+            print('Single click')
+        elif self.click_count > 1:
+            print('Double click')
+        self.click_count = 0
+
+    def __call__(self):
+        self.click_count += 1
+        if not self.timer.isActive():
+            self.timer.start()
